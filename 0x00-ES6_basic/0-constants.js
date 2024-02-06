680eac9296cca84a3dd0aabad2f17d59443f53fb
Fatimazohra95 <fatimazohralakhal32@gmail.com>
2024-02-06 00:17:13 +0000

    Updt

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -3,15 +3,14 @@ export function taskFirst() {
   return task;
 }
 
-export function taskNext() {
-  let combination = 'But sometimes let';
-  combination += getLast(); // Make sure to define getLast() function if it's used here
-
-  return combination;
-}
-
-// Export getLast if it's used in taskNext
 export function getLast() {
   return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
 }
 
--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,5 +1,3 @@
 import { taskFirst, taskNext } from './0-constants.js';
 
-console.log(taskFirst());
-console.log(taskNext());
-
+console.log(`${taskFirst()} ${taskNext()}`);
